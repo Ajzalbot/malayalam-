ec3160d198b6b2926857591dde3a5cd51a1dddab
Ajzalbot <92222418+Ajzalbot@users.noreply.github.com>
2021-10-15 18:50:58 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -9,16 +9,16 @@ class Translation(object):
 </i>"""    
     
     HELP_TEXT = """
-<i>Sorry, I Can't Help You!
+<i>🤧നീ എന്തിനാ ഇതിൽ click ചെയ്‌തെന്ന് എനിക്കറിയാം but ഞാൻ പറയില്ല എനിക്കു നാണമാ🙈... കിട്ടുന്ന MOVIES DOWNLOAD ചെയ്തോണ്ട് പോയിക്കോ നാട്ടുകാരെ കൊണ്ട് അതും ഇതും പറയിക്കാതെ🤣...
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/ajzalk">@ajzal</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/ajzalk">ajzal</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://t.me/MalluSearchBot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/kurachkanjiedukkatte/4">Click Me</a></i>
 """
